cd987fc54825446d8983d0d359ac086d3b220aad
Arne <arnev.c.23@gmail.com>
2024-03-13 11:41:27 +0100

    add agent script to agent

---
+++ Assets/Scripts/CubeAgent.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using
+
+using Unity.MLAgents
+
+using Unity.MLAgents.Sensors
+
+using Unity.MLAgents.Actuators
+
+using System;
+
+public class CubeAgent : Agent
+{
+    public Transform Target;
+    public float speedMultiplier = 0.1f;
+
+    public override void OnEpisodeBegin()
+    {
+        // reset de positie en orientatie als de agent gevallen is
+        if (this.transform.localPosition.y < 0)
+        {
+            this.transform.localPosition = new Vector3(0, 0.5f, 0);
+            this.transform.localRotation = Quaternion.identity;
+        }
+        // verplaats de target naar een nieuwe willekeurige locatie
+        Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    CollectObservations(VectorSensor sensor)
+    {
+        // Target en Agent posities
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Acties, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        transform.Translate(controlSignal * speedMultiplier);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        // Target reached
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // Fallen off the platform?
+        else if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+}
