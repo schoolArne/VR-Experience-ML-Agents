ff2c59e9bfaa734ea717519de9955e697b1ee262
Arne <arnev.c.23@gmail.com>
2024-03-13 11:43:19 +0100

    add missing ';'s

--- Assets/Scripts/CubeAgent.cs
+++ Assets/Scripts/CubeAgent.cs
@@ -1,14 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using
-
-using Unity.MLAgents
-
-using Unity.MLAgents.Sensors
-
-using Unity.MLAgents.Actuators
-
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 using System;
 
 public class CubeAgent : Agent
